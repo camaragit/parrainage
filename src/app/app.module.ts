db806144103d9a10664585e3013bffabbf399de6
Dame <camaradame@gmail.com>
2018-09-05 12:45:48 +0000

    second commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,5 @@
 import { BrowserModule } from '@angular/platform-browser';
-import { ErrorHandler, NgModule } from '@angular/core';
+import {ErrorHandler, Injectable, Injector, NgModule} from '@angular/core';
 import { IonicApp, IonicErrorHandler, IonicModule } from 'ionic-angular';
 import { SplashScreen } from '@ionic-native/splash-screen';
 import { StatusBar } from '@ionic-native/status-bar';
@@ -15,7 +15,34 @@ import {Camera} from "@ionic-native/camera";
 import {FTP} from "@ionic-native/ftp";
 import {ApiProvider} from "../providers/api/api";
 import {GlobalVariableProvider} from "../providers/gloabal-variable/gloabal-variable";
+import {Pro} from "@ionic/pro";
+import {File} from "@ionic-native/file";
+import {FilePath} from "@ionic-native/file-path";
 
+Pro.init('d1ec8226', {
+  appVersion: '0.0.1'
+})
+
+@Injectable()
+export class MyErrorHandler implements ErrorHandler {
+  ionicErrorHandler: IonicErrorHandler;
+
+  constructor(injector: Injector) {
+    try {
+      this.ionicErrorHandler = injector.get(IonicErrorHandler);
+    } catch(e) {
+      // Unable to get the IonicErrorHandler provider, ensure
+      // IonicErrorHandler has been added to the providers list below
+    }
+  }
+
+  handleError(err: any): void {
+    Pro.monitoring.handleNewError(err);
+    // Remove this if you want to disable Ionic's auto exception handling
+    // in development mode.
+    this.ionicErrorHandler && this.ionicErrorHandler.handleError(err);
+  }
+}
 @NgModule({
   declarations: [
     MyApp,
@@ -39,8 +66,10 @@ import {GlobalVariableProvider} from "../providers/gloabal-variable/gloabal-vari
     ApiProvider,
     GlobalVariableProvider,
     WheelSelector,
-    NFC,Toast,Camera,FTP,
-    {provide: ErrorHandler, useClass: IonicErrorHandler}
+    NFC,Toast,Camera,FTP,File,FilePath,
+    {provide: ErrorHandler, useClass: IonicErrorHandler},
+    IonicErrorHandler,
+    [{ provide: ErrorHandler, useClass: MyErrorHandler }]
   ]
 })
 export class AppModule {}
